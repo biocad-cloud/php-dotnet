126f06114bd8009c006f0e7afad02e32a9917546
谢桂纲 <xie.guigang@gcmodeller.org>
2017-03-20 14:32:44 +0800

    vb indexof type

---
+++ Microsoft/VisualBasic/ComponentModel/IndexOf.php
@@ -0,0 +1,39 @@
+<?php
+
+// require('Dictionary.php');
+
+class IndexOf {
+
+    private $__data;
+
+    function __construct($array){
+		
+        $this->__data = new Dictionary;
+        $max = sizeof($array);   
+
+        for($i = 0; $i < $max; $i++) {
+            $this->__data->Add($array[$i], $i);
+        }
+
+        // echo $this->__data->GetJson();
+	}
+
+    /**
+     * 函数返回一个整形数，当目标键名$key在字典之中找不到的时候，会返回-1 
+     */
+    public function IndexOf($key) {
+
+        if($this->__data->ContainsKey($key)) {
+            return $this->__data->Item($key);
+        } else {
+            return -1;
+        }
+
+    }
+
+    public function GetJson() {
+        return $this->__data->GetJson();
+    }
+}
+
+?>
