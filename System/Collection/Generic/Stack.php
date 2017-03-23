6a7e5deb1d983fa578018305f6669409afceef09
xieguigang <xie.guigang@gmail.com>
2017-03-23 19:14:17 +0800

    code comments

--- System/Collection/Generic/Stack.php
+++ System/Collection/Generic/Stack.php
@@ -2,6 +2,9 @@
 
 dotnet::Imports("System.Collection.ICollection");
 
+/**
+ * Represents a variable size last-in-first-out (LIFO) collection of instances of the same specified type.
+ */
 class Stack extends ICollection {
 
 
