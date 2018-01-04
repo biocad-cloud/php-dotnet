bbcf0892fcc8032e55be3a8e2e9437c2d1f230a6
xieguigang <xie.guigang@gcmodeller.org>
2018-01-04 11:21:09 +0800

    db model

--- MVC/config.php
+++ MVC/config.php
@@ -1,6 +1,13 @@
 <?php
 
 # config engine for dotnet framework
+class Config {
 
+    public static $DB_name;
+    public static $DB_user;
+    public static $DB_password;
+    public static $DB_host;
+    public static $DB_port;
 
+}
 ?>
--- MVC/model.php
+++ MVC/model.php
@@ -4,8 +4,67 @@
  * MySQL data table model
  */
 class Model {
-	
-	
+    
+    private $database;
+    private $user;
+    private $password;
+    private $host;
+    private $port;
+
+    // 显示mysql表的结构
+    // DESCRIBE TableName
+    function __construct($database, $user, $password, $host = "localhost", $port = 3306) {
+        $this->database = $database;
+        $this->user     = $user;
+        $this->password = $password;
+        $this->host     = $host;
+        $this->port     = $port;
+    }
+
+    public function Describe($tableName) {
+
+    }
 }
 
+class Table {
+
+    private $tableName;
+
+    function __construct($tableName) {
+        $this->tableName = $tableName;
+    }
+
+    // select all
+    public function select() {
+
+    }
+
+    // select but limit 1
+    public function find() {
+
+    }
+
+    /*
+     * @param $assert: The assert array of the where condition.
+     * 
+     */
+    public function where($assert) {
+
+    }
+
+    // insert into
+    public function add($data) {
+
+    }
+
+    // update table
+    public function save($data) {
+
+    }
+
+    // delete from
+    public function delete($data) {
+
+    }
+}
 ?>
