0574c86c0febb3db3b0369de873df2c1b090cf8d
xieguigang <xie.guigang@gcmodeller.org>
2018-01-04 13:10:21 +0800

    add mysqli driver

--- MVC/model.php
+++ MVC/model.php
@@ -11,6 +11,8 @@ class Model {
     private $host;
     private $port;
 
+    private $debug;
+
     // 显示mysql表的结构
     // DESCRIBE TableName
     function __construct($database, $user, $password, $host = "localhost", $port = 3306) {
@@ -19,13 +21,97 @@ class Model {
         $this->password = $password;
         $this->host     = $host;
         $this->port     = $port;
+
+        $this->debug    = false;
     }
+
+    /*
+	 * 使用这个函数来打开和mysql数据库的链接
+	 */
+	private function __init_MySql() {	
+		$db = mysqli_connect(
+			$this->host.":".$this->port,   // 格式要求： ipaddress:port
+			$this->user,
+			$this->password) or die("Database error: ". mysqli_error()); 
+					
+		if (False == $db) {
+			die("Database connection fail!");
+		} else {
+			return $db;
+		}
+	}
 
     public function Describe($tableName) {
-        $SQL = "DESCRIBE $tableName";
+        $SQL = "DESCRIBE `$tableName`;";
+        $mysqli_exec = $mysqli->__init_MySql();                        
+        $schema = $mysqli->ExecuteSQL($mysqli_exec, $SQL);
 
-        
+        return $schema;
     }
+
+    public function exec($SQL) {
+        $mysqli_exec = $mysqli->__init_MySql();                        
+        $out = $mysqli->ExecuteSQL($mysqli_exec, $SQL);
+        return $out;
+    }
+
+    /*
+	 * 执行一条SQL语句，假若SQL语句是SELECT语句的话，有查询结果的时候
+	 * 会返回记录查询结果的数组集合
+	 *
+	 * 但是对于UPDATE，INSERT和DELETE这类的数据修改语句而言，都是直接
+	 * 返回False的，所以执行这类数据修改的操作的时候就不需要获取返回值
+	 * 赋值到变量了
+	 */
+	public function ExecuteSQL($mysql_exec, $SQL) {
+		
+		mysqli_select_db($mysql_exec, $this->database); 
+		mysqli_query($mysql_exec, "SET names 'utf8'");
+
+		$data = mysqli_query($mysql_exec, $SQL); 
+
+		if ($this->debug) {
+			// $this->printCode($SQL);
+		}
+		
+		if($data) {
+			$out = array();
+			
+			while($row = mysqli_fetch_array($data, MYSQLI_ASSOC)) { 
+				array_push($out, $row);
+			}
+			
+			return $out;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * 执行SQL查询然后返回一条数据
+	 *
+	 */
+	public function ExecuteScalar($mysql_exec, $SQL) {
+		
+		mysqli_select_db($mysql_exec, $this->database); 
+		mysqli_query($mysql_exec, "SET names 'utf8'");
+
+		$data = mysqli_query($mysql_exec, $SQL); 
+
+		if ($this->debug) {
+			// $this->printCode($SQL);
+		}
+		
+		if ($data) {
+			
+			// 只返回一条记录数据
+			while($row = mysqli_fetch_array($data, MYSQLI_ASSOC)) { 
+				return $row;
+			}
+		} else {
+			return false;
+		}
+	}
 }
 
 class Table {
@@ -47,6 +133,10 @@ class Table {
 
         # 获取数据库的目标数据表的表结构
         $this->schema = $this->driver->Describe($tableName);
+    }
+
+    public function exec($SQL) {
+        return $this->driver->exec($SQL);
     }
 
     // select all
