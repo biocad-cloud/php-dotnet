285cba5239b4d25d54c452d2d66669fbc5e1aecd
xieguigang <xie.guigang@gcmodeller.org>
2018-01-04 13:27:17 +0800

    sql where

--- MVC/model.php
+++ MVC/model.php
@@ -119,8 +119,9 @@ class Table {
     private $tableName;
     private $driver;
     private $schema;
+    private $condition;
 
-    function __construct($tableName) {
+    function __construct($tableName, $condition = null) {
         $this->tableName = $tableName;
         $this->driver    = dotnet::config;
         $this->driver    = new Model(
@@ -132,7 +133,20 @@ class Table {
         );
 
         # 获取数据库的目标数据表的表结构
-        $this->schema = $this->driver->Describe($tableName);
+        $this->schema    = $this->driver->Describe($tableName);
+        $this->schema    = $this->schemaArray();
+        $this->condition = $condition;        
+    }
+
+    private function schemaArray() {
+        $array = array();
+
+        foreach ($this->schema as $row) {
+            $field = $row["Field"];
+            $array[$field] = $row;
+        }
+
+        return $array;
     }
 
     public function exec($SQL) {
@@ -141,12 +155,51 @@ class Table {
 
     // select all
     public function select() {
+        $table  = $this->tableName;
+        $assert = $this->getWhere();
+        $mysqli_exec = $mysqli->__init_MySql();       
 
+        if ($assert) {
+            $SQL = "SELECT * FROM `$table` WHERE $assert;";
+        } else {
+            $SQL = "SELECT * FROM `$table`;";
+        }
+        
+        return $this->driver->ExecuteScalar($mysqli_exec, $SQL);
+    }
+
+    private function getWhere() {
+        if (!$this->condition || count($this->condition) == 0) {
+            return null;
+        }
+        
+        $assert = array();
+        $schema = $this->schema;
+
+        foreach ($this->condition as $field => $value) {
+            if (array_key_exists($field, $schema)) {
+                $assert = array_push($assert, "`$field` = '$value'");
+            }
+        }
+
+        $assert = join(" ", $assert);
+
+        return $assert;
     }
 
     // select but limit 1
     public function find() {
+        $table  = $this->tableName;
+        $assert = $this->getWhere();
+        $mysqli_exec = $mysqli->__init_MySql();       
 
+        if ($assert) {
+            $SQL = "SELECT * FROM `$table` WHERE $assert LIMIT 1;";
+        } else {
+            $SQL = "SELECT * FROM `$table` LIMIT 1;";
+        }
+        
+        return $this->driver->ExecuteScalar($mysqli_exec, $SQL);
     }
 
     /*
@@ -154,7 +207,8 @@ class Table {
      * 
      */
     public function where($assert) {
-
+        $next = new Table($this->tableName, $assert);
+        return $next;
     }
 
     // insert into
