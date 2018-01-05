98994ec0724c9f98074a457307ff6c64b37bb3b8
xieguigang <xie.guigang@gcmodeller.org>
2018-01-05 14:41:33 +0800

    add demo

--- test/API.php
+++ test/API.php
@@ -1,14 +1,14 @@
 <?php
 
 include "../package.php";
-include "App.php";
+include "REST.php";
 
 dotnet::ShowAllMessage();
+dotnet::$debug = false;
 
 dotnet::Imports("System.Diagnostics.StackTrace");
 dotnet::AutoLoad("etc/config.php");
 
 Control::$debug = false;
-Control::HandleRequest(new App());
-
+Control::HandleRequest(new REST());
 ?>
