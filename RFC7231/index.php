f8ef7d2ff91324e0b96dedf4f122aefc4a98621f
xieguigang <xie.guigang@gcmodeller.org>
2018-01-30 13:08:32 +0800

    show error pages

--- RFC7231/index.php
+++ RFC7231/index.php
@@ -6,6 +6,25 @@ class Error {
 		return dirname(__FILE__) . "/$code.html";
 	}
 	
+	private static function display($code) {
+		header(Error::getRFC($code));
+		echo file_get_contents(Error::getPath($code));
+	}
+	
+	private static function getRFC($code) {
+		switch ($code) {
+			case 404:
+				return "HTTP/1.0 404 Not Found";
+				break;
+			case 403:
+				return "HTTP/1.0 403 Forbidden";
+				break;
+			case 500:
+				return "HTTP/1.0 500 Internal Server Error";
+				break;
+		}
+	}
+	
 	public static function err404() {
 		echo file_get_contents(Error::getPath(404));
 	}
