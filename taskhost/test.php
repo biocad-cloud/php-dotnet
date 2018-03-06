d113ffd2be6c02fc08eeef51976f305b8108c04d
xieguigang <xie.guigang@gcmodeller.org>
2018-03-06 09:56:58 +0800

    $ticks test success

--- taskhost/test.php
+++ taskhost/test.php
@@ -6,6 +6,8 @@ $signals = "/tmp/test_signals.txt";
 $host    = new taskhost($signals, 1);
 $i       = 0;
 
+date_default_timezone_set('UTC');
+
 class test {
 
     public function test233() {
@@ -17,9 +19,10 @@ class test {
 
 $host->run(function() use (&$i) {
 
-    echo $i++ . "\n";   
+    echo $i++ . "\n";
+    echo date("d-m-Y h:i:s") . "\n";    
 
     echo (new test())->test233();
-});
+}, array(1, 8, 9));
 
 ?>
