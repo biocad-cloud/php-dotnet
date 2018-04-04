6977b591a3b8b1e49f603a708fbef21d32c43da4
xieguigang <xie.guigang@gcmodeller.org>
2018-04-04 09:38:55 +0800

    test on namespace

--- test/test.php
+++ test/test.php
@@ -5,12 +5,14 @@ include "../package.php";
 Imports("System.Threading.Thread");
 Imports("php.Utils");
 
+use System\Threading\Thread as Thread;
+
 function sleepTest() {
 
     echo Utils::Now();
 
     # 0.5s
-    Thread.Sleep(500);
+    System\Threading\Thread.Sleep(500);
 
     echo Utils::Now();
 
