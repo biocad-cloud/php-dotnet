9641eb52625655aaf4441fb462d92456b99c8d67
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-07 15:05:20 +0800

    fix for namespace conflicts

--- System/DateTime.php
+++ System/DateTime.php
@@ -1,17 +1,20 @@
 <?php
 
-class DateTime {
+namespace System {
+    
+    class DateTime {
 
-    public static function year() {
-        return date("Y");
-    }
-
-    public static function month() {
-        return date("m");
-    }
-
-    public static function day() {
-        return date("d");
+        public static function year() {
+            return date("Y");
+        }
+    
+        public static function month() {
+            return date("m");
+        }
+    
+        public static function day() {
+            return date("d");
+        }
     }
 }
 
