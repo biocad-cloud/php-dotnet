eefbb0421a585aa2275f9cba1ccbff86d7d6a866
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-07 14:55:08 +0800

    date time

---
+++ System/DateTime.php
@@ -0,0 +1,18 @@
+<?php
+
+class DateTime {
+
+    public static function year() {
+        return date("Y");
+    }
+
+    public static function month() {
+        return date("m");
+    }
+
+    public static function day() {
+        return date("d");
+    }
+}
+
+?>
