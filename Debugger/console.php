37107429f961acb0d5bdf37471f100e2465d8906
谢桂纲 <xie.guigang@gcmodeller.org>
2018-04-19 04:03:57 +0800

    debugger console ?

--- Debugger/console.php
+++ Debugger/console.php
@@ -13,12 +13,31 @@ class console {
 
     }
 
-    public function dump($obj) {
+    public function writeline($s, $args = NULL) {
+        
+    }
 
+    /**
+     * 经过格式化的var_dump输出
+     * 
+     */
+    public function dump($obj) {
+        // var_dump函数并不会返回任何数据，而是直接将结果输出到网页上面了，
+        // 所以在这里为了能够显示出格式化的var_dump结果，在这里前后都
+        // 添加<code>标签。
+        echo "<code><pre>";
+        echo var_dump($o);    
+        echo "</pre></code>";    
     }
 
     public function error($msg) {
 
+    }
+
+    public static function printCode($code) {
+        echo "<code><pre>";
+        echo $code;
+        echo "</pre></code>";
     }
 }
 
--- Debugger/view.php
+++ Debugger/view.php
@@ -32,24 +32,5 @@ class debugView {
 		
 		return $html;
 	}
-
-	/**
-     * 经过格式化的var_dump输出
-     */
-    public static function VarDump($o) {
-
-        // var_dump函数并不会返回任何数据，而是直接将结果输出到网页上面了，
-        // 所以在这里为了能够显示出格式化的var_dump结果，在这里前后都
-        // 添加<code>标签。
-        echo "<code><pre>";
-        $string = var_dump($o);    
-        echo "</pre></code>";    
-    }
-
-    public static function printCode($code) {
-        echo "<code><pre>";
-        echo $code;
-        echo "</pre></code>";
-    }
 }
 ?>
