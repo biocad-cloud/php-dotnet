483e8883c75bb524517870a6ad4d1dd5f45e7c09
xieguigang <xie.guigang@gcmodeller.org>
2018-05-07 10:08:58 +0800

    unit size tool

--- php/Utils.php
+++ php/Utils.php
@@ -102,6 +102,47 @@ class Utils {
         $suffix = self::GetExtensionSuffix($path);
         return Strings::LCase($suffix) == Strings::LCase($ext);
     }
+
+    /**
+	 * 文件数据格式换算
+	*/ 
+	public static function UnitSize($byte) {
+
+	    if ($byte < 1024) {
+	    	$unit="B";
+	    } else if ($byte < 10240) {
+	      	$byte=self::round_dp($byte/1024, 2);
+	      	$unit="KB";
+	    } else if ($byte < 102400) {
+	      	$byte=self::round_dp($byte/1024, 2);
+	      	$unit="KB";
+	    } else if ($byte < 1048576) {
+	      	$byte=self::round_dp($byte/1024, 2);
+	      	$unit="KB";
+	    } else if ($byte < 10485760) {
+	      	$byte=self::round_dp($byte/1048576, 2);
+	      	$unit="MB";
+	    } else if ($byte < 104857600) {
+	      	$byte=self::round_dp($byte/1048576,2);
+	      	$unit="MB";
+	    } else if ($byte < 1073741824) {
+	      	$byte=self::round_dp($byte/1048576, 2);
+	      	$unit="MB";
+	    } else {
+	      	$byte=self::round_dp($byte/1073741824, 2);
+	      	$unit="GB";
+	    }
+
+		$byte .= $unit;
+
+		return $byte;
+	}
+ 
+	private static function round_dp($num, $dp) {		
+	  	$sh = pow(10, $dp);
+	  	return(round($num * $sh) / $sh);
+	}
+
 }
 
 ?>
