85b7b63ec6053b0a5d1ea04e884a2fee55f36eab
xieguigang <xie.guigang@gcmodeller.org>
2018-05-09 14:37:15 +0800

    add view api test

---
+++ test/Console/view_foreach.php
@@ -0,0 +1,48 @@
+<?php
+
+include "../../package.php";
+
+dotnet::AutoLoad();
+
+Imports("MVC.View.foreach");
+Imports("php.Utils");
+
+$list = [];
+
+$list[1] = [
+    "time"   => Utils::Now(),
+    "title"  => "标题1",
+    "color"  => "red",
+    "amount" => "-500"
+];
+
+$list[2] = [
+    "time"   => Utils::Now(),
+    "title"  => "标题2",
+    "color"  => "green",
+    "amount" => "+50000"
+];
+
+$list[3] = [
+    "time"   => Utils::Now(),
+    "title"  => "标题3",
+    "color"  => "red",
+    "amount" => "-5000"
+];
+
+$template = "
+<ul>
+
+    <foreach @balance>
+        <li>@balance[\"time\"] &nbsp; @balance[\"title\"]
+		     <span style='text-align: right; color: @balance[\"color\"]'>@balance[\"amount\"] 元</span>
+	    </li>
+ 	</foreach>
+
+</ul>";
+
+echo json_encode($list) . "\n\n";
+
+echo MVC\Views\ForEachView::InterpolateTemplate($template, ["balance" => $list]);
+
+?>
---
+++ test/README.md
@@ -0,0 +1,5 @@
+This test contains a demo web site
+
+
++ [./Console](./Console/) Test script that running from php commandline environment
++ [./WebApp](./WebApp/) A demo website based on this framework
