28495ec5c114ffe4b1555d2ca6e6c16e2d36f253
xieguigang <xie.guigang@gcmodeller.org>
2018-08-14 11:38:29 +0800

    add code comments

--- php/export.php
+++ php/export.php
@@ -1,17 +1,39 @@
 <?php
 
 	/**
-	 * Get current year value.
+	 * Get current year value
+	 * 
+	 * A full numeric representation of a year, 4 digits, by default
+	 * 
+	 * @param boolean $short A two digit representation of a year,
+	 *      example as: 99 or 03
+	 * 
+	 * @example 1999 or 2003
+	 * 
+	 * @return string
 	*/
-	function year() {
+	function year($short = false) {
 		return date("Y");
 	}
 
 	/**
-	 * Get current month value.
+	 * Get current month value
+	 * 
+	 * Numeric representation of a month, with leading zeros, by default.
+	 * 
+	 * @param boolean $textual A short textual representation of a month, 
+	 *      three letters, example as: Jan through Dec.
+	 * 
+	 * @example 01 through 12
+	 * 
+	 * @return string
 	*/
-	function month() {
-		return date("m");
+	function month($textual = false) {
+		if ($textual) {
+			return date("M");
+		} else {
+			return date("m");
+		}		
 	}
 
 ?>
