e6b24f940133f2e9fdec838db3f96e8e5666efdd
xieguigang <xie.guigang@gcmodeller.org>
2018-08-29 18:41:10 +0800

    split function

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,60 @@ namespace PhpDotNet {
     class bootstrap {
 
         /**
+         * Imports the external library modules at here?
+        */
+        private static function moduleImports($module) {
+            $info = debug_backtrace(); 
+
+            foreach($info as $k => $v) { 
+                // 解析出当前的栈片段信息
+                if (self::isImportsCall($v)) {                        
+                    // 当前的栈信息不是Imports，则可能是调用Imports函数的脚本文件
+                    $file   = $v["file"];
+                    $dir    = dirname($file);
+                    $module = trim($module, "*");
+                    $module = "$dir/$module"; 
+
+                    break;
+                }
+            }
+
+            # 导入该文件夹下的所有模块文件
+            # 这个模式可以不要求是php.NET模块
+            return self::importsAlls($module);
+        }
+
+        private static function getInternalModuleRefer($module) {
+            $module = str_replace(".", "/", $module);             
+
+            # 2018-5-15 假若Imports("MVC.view");
+            # 因为文件结构之中，有一个view.php和view文件夹
+            # 所以在这里会产生冲突
+            # 在linux上面因为文件系统区分大小写，所以可以通过大小写来避免冲突
+            # 但是windows上面却不可以
+            # 在这里假设偏向于加载文件
+
+            $php = PHP_DOTNET . "/{$module}.php";
+
+            # 如果是文件存在，则只导入文件
+            if (file_exists($php)) {
+                $module = $php;
+            } elseif (file_exists($php = PHP_DOTNET . "/$module/index.php")) {
+                # 如果不存在，则使用index.php来进行判断
+                $module = $php;
+            } elseif (is_dir($dir = PHP_DOTNET . "/$module/")) {
+                # 可能是一个文件夹
+                # 则认为是导入该命名空间文件夹下的所有的同级的文件夹文件
+                return ["true" => dirname($module)];
+            }
+
+            return ["false" => $module];
+        }
+
+        /**
          * @param string $module
+         * 
+         * @return string 返回所导入的文件的全路径名
         */
         public static function LoadModule($module) {
             // 因为WithSuffixExtension这个函数会需要依赖小数点来判断文件拓展名，
@@ -13,55 +66,18 @@ namespace PhpDotNet {
             if (\Utils::WithSuffixExtension($module, "php")) {
                 $module = str_replace(".", "/", $module); 
                 $module = PHP_DOTNET . "/{$module}";
-            } else if (\Strings::EndWith($module, "/*")) {
-
-                $info = debug_backtrace(); 
-
-                foreach($info as $k => $v) { 
-                    // 解析出当前的栈片段信息
-                    if (self::isImportsCall($v)) {                        
-                        // 当前的栈信息不是Imports，则可能是调用Imports函数的脚本文件
-                        $file   = $v["file"];
-                        $dir    = dirname($file);
-                        $module = trim($module, "*");
-                        $module = "$dir/$module"; 
-
-                        break;
-                    }
-                }
-
-                # 导入该文件夹下的所有模块文件
-                # 这个模式可以不要求是php.NET模块
-                return self::importsAll($module);
-
+            } else if (\Strings::EndWith($module, "/*")) {                
+                self::moduleImports($module);
             } else {
-                $module = str_replace(".", "/", $module);             
+                list($isdir, $module) = \Utils::Tuple(self::getInternalModuleRefer($module));
 
-                # 2018-5-15 假若Imports("MVC.view");
-                # 因为文件结构之中，有一个view.php和view文件夹
-                # 所以在这里会产生冲突
-                # 在linux上面因为文件系统区分大小写，所以可以通过大小写来避免冲突
-                # 但是windows上面却不可以
-                # 在这里假设偏向于加载文件
-
-                $php = PHP_DOTNET . "/{$module}.php";
-
-                # 如果是文件存在，则只导入文件
-                if (file_exists($php)) {
-                    $module = $php;
-                } elseif (file_exists($php = PHP_DOTNET . "/$module/index.php")) {
-                    # 如果不存在，则使用index.php来进行判断
-                    $module = $php;
-                } elseif (is_dir($dir = PHP_DOTNET . "/$module/")) {
-                    # 可能是一个文件夹
-                    # 则认为是导入该命名空间文件夹下的所有的同级的文件夹文件
-                    return self::importsAll(dirname($module));
+                if (\boolval($isdir)) {
+                    self::importsAlls($module);
+                } else {
+                    self::importsImpl($module);
                 }
-            }        
-
-            self::importsImpl($module);
-
-            // 返回所导入的文件的全路径名
+            }
+        
             return $module;
         }
 
@@ -134,7 +150,7 @@ namespace PhpDotNet {
          * 
          * @param string $directory 包含有module文件的文件夹的路径
         */
-        private static function importsAll($directory) {
+        private static function importsAlls($directory) {
             $files = [];
             $dir   = opendir($directory);
 
