6aff766a9aa05a6647686d0a94ca05eb25c547bd
xieguigang <xie.guigang@gcmodeller.org>
2018-09-16 18:05:03 +0800

    update runtime

--- MySqli/src/phpCode.vb
+++ MySqli/src/phpCode.vb
@@ -1,3 +1,11 @@
-﻿Public Module phpCode
+﻿Imports System.IO
+Imports Oracle.LinuxCompatibility.MySQL.CodeSolution
+Imports Oracle.LinuxCompatibility.MySQL.Reflection.Schema
 
+Public Module phpCode
+
+    Public Function GenerateCode(mysqlDoc As StreamReader) As String
+        Dim tables As Table() = mysqlDoc.LoadSQLDoc
+
+    End Function
 End Module
