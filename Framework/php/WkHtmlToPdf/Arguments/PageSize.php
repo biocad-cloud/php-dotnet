f77651a34cc20240f108efe96be30c361166c80c
xieguigang <xie.guigang@gcmodeller.org>
2018-11-07 16:29:29 +0800

    page size options

--- Framework/php/WkHtmlToPdf/Arguments/PageSize.php
+++ Framework/php/WkHtmlToPdf/Arguments/PageSize.php
@@ -21,26 +21,27 @@ namespace PHP\WkHtmlToPdf\Options {
      * The defined sizes (With setFullPage(True)) are
     */
     class QPrinter {
-        public const A0 = 5;         // 841 x 1189 mm
-        public const A1 = 6;         // 594 x 841 mm
-        public const A2 = 7;         // 420 x 594 mm
-        public const A3 = 8;         // 297 x 420 mm
-        public const A4 = 0;         // 210 x 297 mm, 8.26 x 11.69 inches
-        public const A5 = 9;         // 148 x 210 mm
-        public const A6 = 10;        // 105 x 148 mm
-        public const A7 = 11;        // 74 x 105 mm
-        public const A8 = 12;        // 52 x 74 mm
-        public const A9 = 13;        // 37 x 52 mm
-        public const B0 = 14;        // 1000 x 1414 mm
-        public const B1 = 15;        // 707 x 1000 mm
-        public const B2 = 17;        // 500 x 707 mm
-        public const B3 = 18;        // 353 x 500 mm
-        public const B4 = 19;        // 250 x 353 mm
-        public const B5 = 1;         // 176 x 250 mm, 6.93 x 9.84 inches
-        public const B6 = 20;        // 125 x 176 mm
-        public const B7 = 21;        // 88 x 125 mm
-        public const B8 = 22;        // 62 x 88 mm
-        public const B9 = 23;        // 33 x 62 mm
+
+        public const A0        = 5;  // 841 x 1189 mm
+        public const A1        = 6;  // 594 x 841 mm
+        public const A2        = 7;  // 420 x 594 mm
+        public const A3        = 8;  // 297 x 420 mm
+        public const A4        = 0;  // 210 x 297 mm, 8.26 x 11.69 inches
+        public const A5        = 9;  // 148 x 210 mm
+        public const A6        = 10; // 105 x 148 mm
+        public const A7        = 11; // 74 x 105 mm
+        public const A8        = 12; // 52 x 74 mm
+        public const A9        = 13; // 37 x 52 mm
+        public const B0        = 14; // 1000 x 1414 mm
+        public const B1        = 15; // 707 x 1000 mm
+        public const B2        = 17; // 500 x 707 mm
+        public const B3        = 18; // 353 x 500 mm
+        public const B4        = 19; // 250 x 353 mm
+        public const B5        = 1;  // 176 x 250 mm, 6.93 x 9.84 inches
+        public const B6        = 20; // 125 x 176 mm
+        public const B7        = 21; // 88 x 125 mm
+        public const B8        = 22; // 62 x 88 mm
+        public const B9        = 23; // 33 x 62 mm
         public const B10       = 16; // 31 x 44 mm
         public const C5E       = 24; // 163 x 229 mm
         public const Comm10E   = 25; // 105 x 241 mm, U.S. Common 10 Envelope
@@ -52,9 +53,43 @@ namespace PHP\WkHtmlToPdf\Options {
         public const Letter    = 2;  // 8.5 x 11 inches, 215.9 x 279.4 mm
         public const Tabloid   = 29; // 279.4 x 431.8 mm
         public const Custom    = 30; // Unknown, Or a user defined size.
+
+        /** 
+         * QPrinter to string
+        */
+        public static function ToString($value) {
+
+        }
     }
 
     class PageSize extends \System\TObject {
 
+        /**
+         * 如果这个参数为<see cref="QPrinter.Custom"/>，则还需要指定width和height
+         * 
+         * @var string
+         * @argv --page-size as string
+        */
+        public $pagesize = QPrinter::A4;
+
+        /**
+         * @argv --page-width as double
+        */        
+        public $pagewidth;
+
+        /** 
+         * @argv --page-height as double
+        */
+        public $pageheight;
+
+        public function ToString() {
+            $pagesize = \PHP\WkHtmlToPdf\Options\QPrinter::ToString($this->pagesize);
+
+            if ($this->pagesize == QPrinter::Custom) {
+                return "--page-size \"{$pagesize}\" --page-width {$this->pagewidth} --page-height {$this->pageheight}";
+            } else {
+                return "--page-size \"{$pagesize}\"";
+            }
+        }
     }
 }
