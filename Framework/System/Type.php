2d41b3745e148c4b680af57d6cec67187e46916c
xieguigang <xie.guigang@gcmodeller.org>
2018-11-07 14:49:29 +0800

    fix for primitive type

--- Framework/System/Type.php
+++ Framework/System/Type.php
@@ -31,6 +31,32 @@ namespace System {
         var $reflector;
 
         /** 
+         * PHP supports ten primitive types.
+         * 
+         * > http://php.net/manual/en/language.types.intro.php
+        */
+        public static $primitiveTypes = [
+#region "Four scalar types:"
+            "boolean"  => false,
+            "integer"  => 0,
+            # float is equals to double
+            "float"    => 0.0,
+            "double"   => 0.0,
+            "string"   => "",
+#endregion
+#region "Four compound types:"
+            "array"    => [],
+            "object"   => null,
+            "callable" => null,
+            "iterable" => null,
+#endregion
+#region "And finally two special types:"
+            "resource" => null,
+            "NULL"     => null
+#endregion
+        ];
+
+        /** 
          * 获取一个值，通过该值指示 ``System.Type`` 是否为基元类型之一。
          * 
          * @return boolean 如果 true 为基元类型之一，则为 ``System.Type``；否则为 false。
@@ -49,22 +75,31 @@ namespace System {
          * @param string $methodName 包含要获取的公共方法的名称的字符串。
         */
         public function GetMethod(string $methodName) {
-            return $this->reflector->getMethod($methodName);
+            if (empty($this->reflector)) {
+                return null;
+            } else {
+                return $this->reflector->getMethod($methodName);
+            }
         }
 
         /**
          * @return \System\Reflection\PropertyInfo[]
         */
         public function GetProperties() {
-            $list = $this->reflector->getProperties();
-            $out  = [];
-            $p    = null;
-
-            foreach($list as $property) {
-                array_push($out, new Property($property, $this));
+            if (empty($this->reflector)) {
+                # 基础类型无方法
+                return [];
+            } else {
+                $list = $this->reflector->getProperties();
+                $out  = [];
+                $p    = null;
+    
+                foreach($list as $property) {
+                    array_push($out, new Property($property, $this));
+                }
+    
+                return $out;
             }
-
-            return $out;
         }
 
         /** 
@@ -102,12 +137,19 @@ namespace System {
         */
         public static function GetClass(string $className) {
             $type = new Type();
-            // 这里返回来的其实是full name
-            // 需要做一下解析
-            $type->reflector = new \ReflectionClass($className);
-            $type->Namespace = $type->reflector->getNamespaceName();
-            $type->Name      = $type->reflector->getShortName();
-            
+
+            if (array_key_exists($className, self::$primitiveTypes)) {
+                # 是基础类型
+                $type->Namespace = "\\";
+                $type->Name      = $className;
+            } else {
+                // 这里返回来的其实是full name
+                // 需要做一下解析
+                $type->reflector = new \ReflectionClass($className);
+                $type->Namespace = $type->reflector->getNamespaceName();
+                $type->Name      = $type->reflector->getShortName();
+            }
+
             return $type;
         }
     }
