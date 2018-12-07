7a2b76a405d2fe8dc2c50cf751b2f632c7e23a16
xieguigang <xie.guigang@gcmodeller.org>
2018-12-07 14:06:54 +0800

    fix a possible bug in path extension function

--- Framework/System/IO/Path.php
+++ Framework/System/IO/Path.php
@@ -20,10 +20,20 @@ class Path {
 
 	/** 
 	 * Get extension name from a given file path
+	 * 
+	 * @param string The given file path.
+	 * 
+	 * @return string The file extension name. (返回来的拓展名是不带小数点的)
 	*/
 	public static function GetExtensionName($filePath) {
-		$array = explode('.', $filePath);
-		$extension = end($array);
-		return $extension;
+		$array = explode("/", str_replace("\\", "/", $filePath));
+		$array = explode('.', end($array));
+
+		if (count($array) == 1) {
+			return "";
+		} else {
+			$extension = end($array);
+			return $extension;
+		}
 	}
 }
