5a234fc38ed550dd1665dfd824eed7b9c2501496
xieguigang <xie.guigang@gcmodeller.org>
2018-12-29 14:32:34 +0800

    parse url query

--- Framework/php/URL.php
+++ Framework/php/URL.php
@@ -48,4 +48,37 @@ class URL {
 
         return implode('&', $tmp);
     }
+
+    /**
+     * UTF-8 aware ``parse_url()`` replacement.
+     * 
+	 * @param string $url
+     * @param boolean $parseURLQuery 是否同时也将query部分解析为数组？默认是不解析，即保持为字符串
+     * @return array
+    */
+    public static function mb_parse_url($url, $parseURLQuery = false) {
+        $enc_url = preg_replace_callback(
+            '%[^:/@?&=#]+%usD',
+            function ($matches) {
+                return urlencode($matches[0]);
+            },
+            $url
+        );
+        
+        $parts = parse_url($enc_url);
+        
+        if ($parts === false) {
+            throw new \InvalidArgumentException("Malformed URL: $url");
+        }
+        
+        foreach($parts as $name => $value) {
+            $parts[$name] = urldecode($value);
+        }
+        
+        if ($parseURLQuery && array_key_exists("query", $parts)) {
+            $parts["query"] = self::ConvertUrlQuery($parts["query"]);
+        }
+
+        return $parts;
+    }
 }
--- test/Console/routerTest.php
+++ test/Console/routerTest.php
@@ -3,8 +3,9 @@
 include __DIR__ . "/../../package.php";
 
 Imports("php.htaccess");
+Imports("php.URL");
 Imports("MVC.router");
 
-$url = "dict.php?app=search&q=browse&FORM=BDVSP6&mkt=zh-cn";
+$url = "/dict.php?app=search&q=browse&FORM=BDVSP6&mkt=zh-cn";
 
-console::dump(Router::mb_parse_url($url));
+console::dump(URL::mb_parse_url($url));
