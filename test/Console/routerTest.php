b12d1d58fff133be7a279e03ba76507992191028
xieguigang <xie.guigang@gcmodeller.org>
2018-12-29 16:53:33 +0800

    test success

--- test/Console/routerTest.php
+++ test/Console/routerTest.php
@@ -36,5 +36,13 @@ console::log("URL pattern match test:");
 $test1 = URL::mb_parse_url("/dict.php?app=search&q=browse&FORM=BDVSP6&mkt=zh-cn", true, true);
 $test2 = URL::mb_parse_url("/dict.php?app=searchTool&q=browse&FORM=BDVSP6&mkt=zh-cn", true, true);
 
-console::log($rule->MatchRule($test1));
-console::log($rule->MatchRule($test2));
+# rule是使用通配符匹配任意app
+# 下面的两个匹配肯定被判断为相同
+console::log(intval($rule->MatchRule($test1)));
+console::log(intval($rule->MatchRule($test2)));
+
+# rule是使用一个固定的常数字符串
+# 下面的两个匹配有一个不相同
+$rule2 = new \PHP\RewriteRule("^dict/(\S+)?q=(.+)&FORM=(.+)", "/dict.php?app=searchTool&q=$2&FORM=$3");
+console::log(intval($rule2->MatchRule($test1)));
+console::log(intval($rule2->MatchRule($test2)));
