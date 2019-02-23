0dbeee6deccd842bb0e210acdb5025a1f7783c26
xieguigang <xie.guigang@gcmodeller.org>
2019-02-23 11:36:25 +0800

    test create variable success

--- test/WebApp/view/index.php
+++ test/WebApp/view/index.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 class app {
 
 
@@ -9,6 +11,13 @@ public static function fakeController() {
     $gg = "GGGGGGGGGGGG";
     $name = "12345FFFFF";
 
+    $name = "XX";
+
+    # 可以通过下面的表达式动态的创建一个变量
+${$name} = "hello world!";
+
+
+echo var_dump($XX);
 
     include "./test1.phtml";
 }
