c8522678a1e8e5947cae51cb2b01c1230ec020b7
xieguigang <xie.guigang@gcmodeller.org>
2019-09-12 12:07:03 +0800

    cache module

--- Framework/MVC/view.php
+++ Framework/MVC/view.php
@@ -213,26 +213,6 @@ class View {
 	}
 	
 	/**
-	 * 获取目标html文档梭对应的缓存文件的文件路径
-	*/
-	private static function getCachePath($path) {
-		// temp/{yyymmmdd}/viewName
-		$version = filemtime($path);
-		$temp    = dotnet::getMyTempDirectory();
-		# 因为缓存的路径和主html文件的修改时间相关，所以如果只是文档碎片被更新了
-		# 会因为主html文件没有被修改的原因而没有更新cache
-		# 在这里使用app version来更新缓存
-		$appVer  = DotNetRegistry::Read("APP_VERSION", "0.0.0");
-		$file    = basename($path);
-		# 2018-09-18 从下面的代码之中可以看见，因为缓存页面是和用户请求有关的
-		# 所以没有办法为每一个视图页面生成缓存页面
-		$path    = md5($_SERVER["REQUEST_URI"]);
-		$cache   = "$temp/$appVer/$file/$version/$path.html";
-
-		return $cache;
-	}
-
-	/**
 	 * 加载指定路径的html文档并对其中的占位符利用vars字典进行填充
 	 * 这个函数还会额外的处理includes关系
 	 * 
@@ -255,6 +235,10 @@ class View {
 
 		if (file_exists($path)) {
 			$html = self::loadTemplate($path, $lang);
+
+			if (APP_DEBUG && strlen($html) == 0) {
+				console::warn("The raw template data is empty! Please check for file <strong>$path</strong>.");
+			}
 		} else {
 			# 给出文件不存在的警告信息
 			return "HTML document view <strong>&lt;$path></strong> could not be found!";
@@ -322,43 +306,9 @@ class View {
 		$usingCache = DotNetRegistry::Read("CACHE", false);
 
 		if ($usingCache && !Strings::Empty($path)) {
-			# 在配置文件之中开启了缓存选项
-			$cache = self::getCachePath($path);
+			Imports("MVC.View.cache");
 
-			# 在调试模式下总是不使用cache
-			# 为了将cache的信息也输出到调试终端，在这里设置条件为调试模式或者缓存文件
-			# 不存在都会进行缓存的生成
-			if (APP_DEBUG || (!file_exists($cache)) || (filesize($cache) <= 0)) {
-				# 当缓存文件不存在的时候，生成缓存，然后返回
-				
-				# 将html片段合并为一个完整的html文档
-				# 得到了完整的html模板
-				$html      = file_get_contents($path);
-
-				$cachePage = View::interpolate_includes($html, $path);
-				$cachePage = View::valueAssign($cachePage, $language);
-
-				if (DotNetRegistry::HtmlMinifyOfCache()) {
-					# 进行html文件的压缩
-					$cachePage = \MVC\Views\HtmlMinifier::minify($cachePage);
-				}
-
-				$cacheDir  = dirname($cache);
-				
-				if (!file_exists($cacheDir)) {
-					mkdir($cacheDir, 0777, true);
-				}				
-				file_put_contents($cache, $cachePage);
-
-				debugView::LogEvent("HTML view cache created!");
-			} else {
-				debugView::LogEvent("HTML view cache hits!");
-			}
-
-			$cache = realpath($cache);			
-			$html  = file_get_contents($cache);
-
-			debugView::LogEvent("Cache=$cache");
+			list($html, $cache) = ViewCache::doCache($path);
 		} else {
 			$cache = 'disabled';
 			# 不使用缓存，需要进行页面模板的拼接渲染
@@ -384,6 +334,8 @@ class View {
 		$name = pathinfo($path);
 		$name = $name['filename'];
 		$lang = dirname($path) . "/$name.$lang.php";		
+
+		\debugView::LogEvent("Language file for current view: <strong>$lang</strong>.");
 
 		if (!file_exists($lang)) { 
 			return $vars;
@@ -447,7 +399,7 @@ class View {
 	 * Create user html document based on the html template 
 	 * and the given configuration data.
 	*/
-	public static function InterpolateTemplate($html, $vars) {		
+	public static function InterpolateTemplate($html, $vars) {
 		# 没有需要进行设置的变量字符串，则直接在这里返回html文件
 		if (!$vars && !self::$join) {
 			# 假设在html文档里面总是会存在url简写的，
