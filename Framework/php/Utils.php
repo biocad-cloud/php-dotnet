3b347e03125a567c606fec27fa485b44a6e7c0d5
xieguigang <xie.guigang@gcmodeller.org>
2019-09-24 12:54:14 +0800

    improvements of the empty array determination

--- Framework/php/Utils.php
+++ Framework/php/Utils.php
@@ -421,7 +421,7 @@ class Utils {
      *      返回第一个被查找到的键名的值，这个表达式通常用来表示别名查找
     */
     public static function ReadValue($array, $key, $default = null) {
-        if (empty($array)) {
+        if ($array == false || is_null($array) || empty($array)) {
             return $default;
         } else if (is_array($array)) {
 
