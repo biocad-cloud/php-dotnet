a41f10915f30a1a82a2c662b75aa7481fcced728
xieguigang <xie.guigang@gcmodeller.org>
2019-10-30 09:04:34 +0800

    order_by test success

--- test/Console/mysql_queryBuilder.php
+++ test/Console/mysql_queryBuilder.php
@@ -4,8 +4,15 @@ include __DIR__ . "/../../package.php";
 
 dotnet::AutoLoad();
 
+Imports("MVC.MySql.debuggerDriver");
+
 $table = Table::GetDebugger("program", "biodeep_workspace", __DIR__ . "/etc/workspace.php");
 
-$table->where(["id" => gt_eq(500)])->order_by("app_name desc")->limit(5,5)->select();
+$table->where([
+    "version" => gt_eq(500),
+    "id" => between(900,2000)
+])->order_by("app_name desc")
+  ->limit(5,55)
+  ->select();
 
 console::log($table->getLastMySql());
