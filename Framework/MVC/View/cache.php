0492d05700020b7425f30bb2906f9af6a38110aa
xieguigang <xie.guigang@gcmodeller.org>
2020-11-10 21:06:21 +0800

    Update cache.php

--- Framework/MVC/View/cache.php
+++ Framework/MVC/View/cache.php
@@ -4,49 +4,15 @@ class ViewCache {
     
     public static function doCache($path, $language) {
         # 在配置文件之中开启了缓存选项
-        $cache = self::getCachePath($path);
+        $cache = self::getCachePath($path, $language);
 
         # 在调试模式下总是不使用cache
         # 为了将cache的信息也输出到调试终端，在这里设置条件为调试模式或者缓存文件
         # 不存在都会进行缓存的生成
         if (APP_DEBUG || (!file_exists($cache)) || (filesize($cache) <= 0)) {
-            # 当缓存文件不存在的时候，生成缓存，然后返回
-            console::log("Cache file will save at: <code>$cache</code>");
+            self::createViewCache($cache, $language, $path);
 
-            # 将html片段合并为一个完整的html文档
-            # 得到了完整的html模板
-            $html      = file_get_contents($path);
-
-            console::log("Template size = <strong>" . strlen($html) . "</strong> characters.");
-
-            $cachePage = View::interpolate_includes($html, $path);
-            $cachePage = View::valueAssign($cachePage, $language);
-
-            console::log("Cache data created!");
-            console::log("sizeof cache is " . strlen($cachePage));
-
-            if ((!APP_DEBUG) && DotNetRegistry::HtmlMinifyOfCache()) {
-                # 进行html文件的压缩
-                $cachePage = \MVC\Views\HtmlMinifier::minify($cachePage);                
-            }
-
-            $cacheDir  = dirname($cache);
-            
-            # 20191201 当web服务器对缓存文件夹没有写入权限的时候
-            # 会因为cache文件不存在而无法显示出页面
-            # 产生空白页面的bug现象
-            if (!file_exists($cacheDir)) {
-                mkdir($cacheDir, 0777, true);
-            }
-            file_put_contents($cache, $cachePage);
-
-            if (!file_exists($cache)) {
-                # 缓存文件没有写入成功
-                # 可能是缓存文件夹所在的位置没有写入权限
-                # 给出错误消息
-                throw new Exception("Cache location '$cache' is not writable!");
-            }
-
+            # debug logging
             debugView::LogEvent("HTML view cache created!");
         } else {
             debugView::LogEvent("HTML view cache hits!");
@@ -61,9 +27,50 @@ class ViewCache {
     }
 
     /**
+     * 当缓存文件不存在的时候，生成缓存，然后返回
+    */
+    private static function createViewCache($cache, $language, $path) {
+        # 将html片段合并为一个完整的html文档
+        # 得到了完整的html模板
+        $html = file_get_contents($path);
+
+        console::log("Cache file will save at: <code>$cache</code>");
+        console::log("Template size = <strong>" . strlen($html) . "</strong> characters.");
+
+        $cachePage = View::interpolate_includes($html, $path);
+        $cachePage = View::valueAssign($cachePage, $language);
+
+        console::log("Cache data created!");
+        console::log("sizeof cache is " . strlen($cachePage));
+
+        if ((!APP_DEBUG) && DotNetRegistry::HtmlMinifyOfCache()) {
+            # 进行html文件的压缩
+            $cachePage = \MVC\Views\HtmlMinifier::minify($cachePage);                
+        }
+
+        $cacheDir = dirname($cache);
+        
+        # 20191201 当web服务器对缓存文件夹没有写入权限的时候
+        # 会因为cache文件不存在而无法显示出页面
+        # 产生空白页面的bug现象
+        if (!file_exists($cacheDir)) {
+            mkdir($cacheDir, 0777, true);
+        }
+
+        file_put_contents($cache, $cachePage);
+
+        if (!file_exists($cache)) {
+            # 缓存文件没有写入成功
+            # 可能是缓存文件夹所在的位置没有写入权限
+            # 给出错误消息
+            throw new Exception("Cache location '$cache' is not writable!");
+        }
+    }
+
+    /**
 	 * 获取目标html文档梭对应的缓存文件的文件路径
 	*/
-	private static function getCachePath($path) {
+	private static function getCachePath($path, $language) {
 		// temp/{yyymmmdd}/viewName
 		$version = filemtime($path);
 		$temp    = dotnet::getMyTempDirectory();
@@ -77,9 +84,9 @@ class ViewCache {
         # 所以没有办法为每一个视图页面生成缓存页面
         if (IS_CLI) {
             # may be have some task for rendering view page in cli app
-            $path = md5(join(" ", $_SERVER['argv']));
+            $path = md5(join(" ", $_SERVER['argv']) . "lang=$language");
         } else {
-            $path = md5($_SERVER["REQUEST_URI"]);
+            $path = md5($_SERVER["REQUEST_URI"] . "lang=$language");
         }
 		
 		$cache = "$temp/$appVer/$file/$version/$path.html";
