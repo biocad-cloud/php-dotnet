61697d6804c46f10bdb5b960966078db359206a0
guigang xie <xie.guigang@gcmodeller.org>
2023-03-20 10:44:27 +0800

    Create sync_multiplegit.cmd

---
+++ .github/sync_multiplegit.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+REM git remote add gitee https://gitee.com/xieguigang/php-dotnet.git
+
+REM git pull local HEAD
+git pull gitee HEAD
+
+REM git push local HEAD
+git push gitee HEAD
+
+echo synchronization of this code repository job done!
